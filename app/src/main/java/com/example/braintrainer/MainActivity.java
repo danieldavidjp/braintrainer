17fb2dfc6e5152aacb315730ff7b69e2dad527b2
Daniel Jimenez <danieldavidjp@gmail.com>
2020-08-06 22:11:27 -0500

    Main Activity butttons

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void chooseAnswer
-
     Button startButton;
+    ArrayList<Integer> answers = new ArrayList<>();
+
+    public void chooseAnswer(View view){
+    }
 
     public void startGame(View view){
 
@@ -22,6 +28,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView sumTextView = findViewById(R.id.sumTextView);
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+
         startButton = findViewById(R.id.startButton);
+
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + "+" + Integer.toString(b));
+
+        int locationAnswer = rand.nextInt(4);
+
+        for(int i=0; i<4; i++){
+
+            if( i == locationAnswer){
+                answers.add(a+b);
+            }else {
+                int wrongAnswer = rand.nextInt(41);
+
+                while (wrongAnswer == a+b){
+
+                    wrongAnswer = rand.nextInt(41);
+                }
+                answers.add(wrongAnswer);
+            }
+
+        }
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
     }
 }
